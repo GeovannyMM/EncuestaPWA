d68c6b4561ee19ed7242a968d090ea7192f2d7f9
GeovannyMM <jgmedina117@gmail.com>
2026-03-09 22:21:45 -0600

    db export const db, solo eso

--- lib/shared/validators.ts
+++ lib/shared/validators.ts
@@ -1,27 +1,27 @@
 import { z } from "zod";
 
 export const usuarioSchema = z.object({
-    id: z.number(),
-    nombre_completo: z.string(),
-    usuario_slug: z.string(),
+  id: z.number(),
+  nombre_completo: z.string(),
+  usuario_slug: z.string(),
 });
 export type Usuario = z.infer<typeof usuarioSchema>;
 
-
 // La palabra 'export' es CLAVE para que otros archivos lo vean
 export const encuestaSchema = z.object({
-    entrevistador: z.number(),
-    folio: z.string(),
-    nombreEncuestado: z.string().min(5),
-    ubicacion: z.object({
-        lat: z.number(),
-        lng: z.number(),
-    }).optional(),
-    respuestas: z.record(z.string(), z.any()),
-    fechaHora: z.string(),
-    estado_sinc: z.boolean(),
+  entrevistador: z.number(),
+  folio: z.string(),
+  nombreEncuestado: z.string().min(5),
+  ubicacion: z
+    .object({
+      lat: z.number(),
+      lng: z.number(),
+    })
+    .optional(),
+  respuestas: z.record(z.string(), z.any()),
+  fechaHora: z.string(),
+  estado_sinc: z.boolean(),
 });
 
 // Exportamos el Tipo para que Dexie sepa qué forma tienen los datos
 export type Encuesta = z.infer<typeof encuestaSchema>;
-
